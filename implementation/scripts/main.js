0b8ce9b414a32929d133816558bc11a68caadecb
James Jackson <james.ogge@gmail.com>
2017-02-14 09:17:33 +0000

    setting up

---
+++ implementation/scripts/main.js
@@ -0,0 +1,46 @@
+function drawRectangle(ctx, angle){
+  ctx.save();
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  if(typeof angle !== "undefined" || angle !== 0){
+    ctx.rotate(degToRad(angle));
+  }
+  ctx.fillRect(0, 0, 10, 50);
+  ctx.restore();
+}
+function init(){
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext("2d");
+  parseGrammar(ctx, "106AA11B22A22C");
+}
+function parseGrammar(ctx, grammar){
+  var re = /([0-9]*[A-Z]{1})/gi;
+  gram = grammar.match(re);
+  console.log(gram.length);
+  gram.forEach(function(el){
+    console.log(el);
+  });
+
+
+  // for (var i = 0; i < grammar.length; i++) {
+  //   var angle = 0;
+  //   switch (grammar[i]) {
+  //     case "B":
+  //       angle = 45;
+  //       break;
+  //     case "C":
+  //       angle = 90;
+  //       break;
+  //     case "D":
+  //       angle = 135;
+  //     default:
+  //
+  //   }
+  //   drawRectangle(ctx, angle);
+  // }
+}
+function degToRad(angle){
+  return angle * (Math.PI / 180);
+}
+window.onload = function(){
+  init();
+}
