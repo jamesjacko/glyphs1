c834227eea65549f7e4a68f48cc8e38545f2c7e2
James Jackson <james.ogge@gmail.com>
2017-02-14 10:58:50 +0000

    rotating points

--- implementation/scripts/main.js
+++ implementation/scripts/main.js
@@ -1,46 +1,70 @@
-function drawRectangle(ctx, angle){
-  ctx.save();
-  ctx.translate(canvas.width / 2, canvas.height / 2);
-  if(typeof angle !== "undefined" || angle !== 0){
-    ctx.rotate(degToRad(angle));
-  }
-  ctx.fillRect(0, 0, 10, 50);
-  ctx.restore();
-}
+
 function init(){
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext("2d");
-  parseGrammar(ctx, "106AA11B22A22C");
-}
-function parseGrammar(ctx, grammar){
-  var re = /([0-9]*[A-Z]{1})/gi;
-  gram = grammar.match(re);
-  console.log(gram.length);
-  gram.forEach(function(el){
-    console.log(el);
+  draw(ctx);
+};
+
+function draw(ctx){
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  var size = 80;
+  var offset = 20;
+  ctx.rect(offset, offset, size, size);
+  ctx.stroke();
+  var coords = initCoords(size);
+  var pivot = {
+    x: (size / 2),
+    y: (size / 2)
+  }
+  coords.forEach(function(elem){
+    elem = rotatePoint(pivot, elem, degToRad(105));
+    console.log(elem);
+    ctx.fillRect(offset + elem.x, offset + elem.y, 2, 2);
   });
+}
+/**
+ *  Returns normalized coordinates for initial line
+ */
 
+function initCoords(size){
+  var points = [
+    {
+      x: 0.1 * size,
+      y: 0.5 * size
+    },
+    {
+      x: 0.35 * size,
+      y: 0.5 * size
+    },
+    {
+      x: 0.65 * size,
+      y: 0.5 * size
+    },
+    {
+      x: 0.9 * size,
+      y: 0.5 * size
+    }
+  ];
+  return points;
+}
 
-  // for (var i = 0; i < grammar.length; i++) {
-  //   var angle = 0;
-  //   switch (grammar[i]) {
-  //     case "B":
-  //       angle = 45;
-  //       break;
-  //     case "C":
-  //       angle = 90;
-  //       break;
-  //     case "D":
-  //       angle = 135;
-  //     default:
-  //
-  //   }
-  //   drawRectangle(ctx, angle);
-  // }
+function rotatePoint(pivot, point, angle){
+  var s = Math.sin(angle);
+  var c = Math.cos(angle);
+  point.x -= pivot.x;
+  point.y -= pivot.y;
+
+  var x = (point.x * c - point.y * s) + pivot.x;
+  var y = (point.x * s - point.y * c) + pivot.y;
+
+  point.x = x;
+  point.y = y;
+
+  return point;
 }
 function degToRad(angle){
   return angle * (Math.PI / 180);
 }
 window.onload = function(){
   init();
-}
+};
