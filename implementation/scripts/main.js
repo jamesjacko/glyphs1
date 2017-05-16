4b0e2588682746d786426427518f2b9e868e08df
James Jackson <james.ogge@gmail.com>
2017-05-16 10:48:23 +0100

    Setting up NPM

--- implementation/scripts/main.js
+++ implementation/scripts/main.js
@@ -1,43 +1,27 @@
-window.onload = function(){
-  var numGlyphTypes = 28;
-  var obj = {
-      name: "Student",
-      values: [{
-          name: "ICP1002",
-          min: 1,
-          max: 100,
-          value: 50
-      },
-      {
-          name: "ICP1015",
-          min: 1,
-          max: 100,
-          value: 50
-      },
-      {
-          name: "ICP1016",
-          min: 1,
-          max: 100,
-          value: 50
-      },
-      {
-          name: "ICP1022",
-          min: 1,
-          max: 100,
-          value: 50
-      }]
-  }
-  var div, canvas;
-  div = document.createElement('div');
-  for (var i = 0; i < numGlyphTypes; i++) {
-      div.classList.add('img_surv_option');
-      canvas = document.createElement('canvas');
-      canvas.width = 100;
-      canvas.height = 100;
-      canvas.style.background = "#FFF";
-      div.appendChild(canvas);
-      getGlyph(canvas, i, obj);
-  }
-  document.getElementById("glyphs").appendChild(div);
+window.onload = function() {
+    generateGlyphs("glyphs", 28);
+    document.getElementById("update").addEventListener("click", function() {
+        generateGlyphs("glyphs", 28);
+    })
+}
 
+function generateGlyphs(id, numGlyphTypes){
+  document.getElementById(id).innerHTML = "";
+  var obj = JSON.parse(document.getElementById("JSON").value);
+  for (var i = 0; i < numGlyphTypes; i++) {
+      generateGlyph("glyphs", i, obj);
+  }
+}
+
+function generateGlyph(id, glyph, obj) {
+    var div, canvas;
+    div = document.createElement('div');
+    div.classList.add('glyph');
+    canvas = document.createElement('canvas');
+    canvas.width = 100;
+    canvas.height = 100;
+    canvas.style.background = "#FFF";
+    div.appendChild(canvas);
+    getGlyph(canvas, glyph, obj);
+    document.getElementById(id).appendChild(div);
 }
