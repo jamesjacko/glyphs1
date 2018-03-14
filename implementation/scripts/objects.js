6512a31bbf169639301b4f42f9dd73222334eff6
Unknown <james.ogge@gmail.com>
2018-03-14 07:17:52 +0000

    Push

---
+++ implementation/scripts/objects.js
@@ -0,0 +1,56 @@
+function getObject(itemLength, range, groups){
+    var obj = {
+        name: "student",
+        groups: [
+            0, 1, 2
+        ],
+        values: []
+    };
+    for(var i = 0; i < itemLength; i++){
+        var value = {
+            name: "l" + i,
+            min: 0,
+            max: 100,
+            value: Math.round(Math.random() * (range.max - range.min) + range.min),
+            group: i % groups,
+            correct: range.max === 40
+        };
+        obj.values.push(value);
+
+    }
+    return obj;
+}
+
+function getRanges(numObjects, targetNumber){
+    var ranges = [];
+    for(var i = 0; i < numObjects; i++){
+        ranges .push({
+            min: i < targetNumber? 0: 60,
+            max: i < targetNumber? 40: 100
+        });
+    }
+    return shuffleArray(ranges);
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function getObjects(num, corrNum){
+    var objs = [];
+    var ranges = getRanges(num, corrNum);
+    for(var i = 0; i < num; i++){
+        objs.push(getObject(9, ranges[i], 3));
+    }
+    return objs;
+}
+
+function setupObjects(){
+    var objectCount = 25;
+    var correctObjectCount = 5;
+    return getObjects(objectCount, correctObjectCount);;
+}
